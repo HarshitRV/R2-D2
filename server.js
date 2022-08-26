fc2a25f20e248af7051c359acd2c1ef82b07e292
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2022-08-26 18:22:03 +0530

    Minor bug fixes

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-import express from "express"
+import express from "express";
 
-const server = express()
+const server = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 server.all("/", (req, res) => {
   res.send({
     status: 200,
     message: "Hello from R2-D2. I am online"
-  })
+  });
 })
 
 export const keepAlive = () => {
   server.listen(PORT, () => {
     console.log("R2-D2 is online")
-  })
+  });
 }
